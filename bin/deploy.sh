5712b5b5ce6da97f02dddb046c033de36648946e
Ivan Roseland <icroseland@gmail.com>
2026-04-07 11:03:30 -0500

    make the dnf quiet

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -81,7 +81,7 @@ echo "rpm -Uvh https://yum.voxpupuli.org/$GET_FILE"
 rpm -Uvh "https://yum.voxpupuli.org/$GET_FILE"
 #disable selinux as its an annoyance for a demo right now.
 ##/usr/sbin/setenforce 0
-dnf install openvox-server -y
+dnf -yq install openvox-server  
 echo 'quick test'
 fi
 echo 'package installed moving on to grab modules'
