c99c69b1d8927947b2ce24b7d20e044a40e40bb1
Ivan Roseland <icroseland@gmail.com>
2026-04-09 17:49:18 -0500

    ok, now we need to install openvox

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -59,10 +59,9 @@ if [ -f /etc/redhat-release ]; then
     echo "LSB eq $DIST_VER  $LSB"
     GET_FILE=`curl -k -s https://yum.voxpupuli.org/ | grep -oP '(?<=href=")[^"]+' | grep -v '^/' | grep "$SN-$LSB" | sort -r | head -n 1`
     echo "rpm -Uvh https://yum.voxpupuli.org/$GET_FILE"
-    ###rpm -i https://yum.voxpupuli.org/$GET_FILE
+    rpm -i https://yum.voxpupuli.org/$GET_FILE
     #disable selinux as its an annoyance for a demo right now.
     ##/usr/sbin/setenforce 0
-    #dnf -yq install openvox-server  
 
     echo 'quick test'
 
