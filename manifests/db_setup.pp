42cbdca719666bf0a62daddaa6cae7e0c8cd247e
Ivan Roseland <icroseland@gmail.com>
2026-04-09 10:56:03 -0500

    fixing facts

--- manifests/db_setup.pp
+++ manifests/db_setup.pp
@@ -1,20 +1,21 @@
 # install puppdb and postgres
 #
 class start_master::db_setup(
-$puppetdb_server = $::fqdn,
-$postgresql_server = $::fqdn
+  $fqdn = $facts['networking']['fqdn'],
+  $puppetdb_server = $fqdn,
+  $postgresql_server = $fqdn
 ){
-if ($puppetdb_server == $::fqdn) and ($postgresql_server == $:fqdn ){
+if ($puppetdb_server == $fqdn) and ($postgresql_server == $fqdn ){
   class { 'puppetdb': }
 }
-if ($puppetdb_server != $::fqdn) and ($postgresql_server == $:fqdn ){
+if ($puppetdb_server != $fqdn) and ($postgresql_server == $fqdn ){
   class { 'puppetdb::database::postgresql':
     listen_addresses => $postgres_server,
     postgresql_ssl_on => true,
     puppetdb_server => $puppetdb_server
     }
 }
-if ($puppetdb_server == $::fqdn) and ($postgresql_server =! $:fqdn ){
+if ($puppetdb_server == $fqdn) and ($postgresql_server != $fqdn ){
   class { 'puppetdb::server':
     database_host => $postgres_server,
     postgresql_ssl_on => true
