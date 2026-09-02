ab8646ae1d085315fc7059b0d5c898c449cd0bec
Ivan Roseland <icroseland@gmail.com>
2026-09-02 21:16:12 +0000

    it is spacing things wierd

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -60,7 +60,8 @@ if [ -f /etc/redhat-release ]; then
     echo "LSB eq $DIST_VER  $LSB"
     GET_FILE=`curl -k -s https://yum.voxpupuli.org/ | grep -oP '(?<=href=")[^"]+' | grep -v '^/' | grep openvox | grep "$SN-$LSB" | sort -r | head -n 1`
     echo "rpm -Uvh https://yum.voxpupuli.org/$GET_FILE"
-    rpm -Uvh  "https://yum.voxpupuli.org/$GET_FILE" &&
+    echo "##############################################"
+    ##rpm -Uvh  "https://yum.voxpupuli.org/$GET_FILE" &&
     #disable selinux as its an annoyance for a demo right now.
     ##/usr/sbin/setenforce 0
     if [ 'rpm -qa | grep openvox-server' ]; then
